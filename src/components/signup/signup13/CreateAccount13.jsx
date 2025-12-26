f30bc073e805cdc6206036a962c45969b7b05c14
amnakhalidpython-code <amnakhalidpython@gmail.com>
2025-12-26 21:26:01 +0500

    port change

--- src/components/signup/signup13/CreateAccount13.jsx
+++ src/components/signup/signup13/CreateAccount13.jsx
@@ -78,7 +78,7 @@ const CreateAccount13 = () => {
 
       console.log('Creating board with data:', boardPayload);
 
-      const response = await fetch('http://localhost:3002/api/boards', {
+      const response = await fetch('http://localhost:5000/api/boards', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
